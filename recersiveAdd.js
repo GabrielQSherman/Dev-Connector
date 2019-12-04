a95f23d92e5f444de4fcf4bb487a596a30820a7d
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-12-04 14:54:49 -0500

    Add files via upload

--- recersiveAdd.js
+++ recersiveAdd.js
@@ -5,19 +5,19 @@ let readline = require('readline'),
     });
 
 rl.question("Enter a number, your output will be that numbers sumation\nNumber: ", (num) => {
+
+    num = parseInt(num);
     
 
     function sum(num) {
         
-        // if (num == 0 ) {
-        //     return 0
-        // } else
+        if (num == 0 ) {
+            return 0
+        } else
          if (num < 0) {
             return num + sum(num + 1);
         } else if (num > 0) {
             return num + sum (num - 1);
-        } else {
-            return 0
         }
     }
 
