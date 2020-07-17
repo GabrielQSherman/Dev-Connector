a466dd5c9dfb281e84229f865e1dd64585483e8e
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-16 23:59:41 -0400

    update comonent

---
+++ client/src/Actions/login.js
@@ -0,0 +1,36 @@
+import api from '../utils/api';
+import { setAlert } from './alert';
+import {
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT
+} from './types';
+
+// Login User
+export const login = (email, password) => async dispatch => {
+    const body = { email, password };
+  
+    try {
+      const res = await api.post('/auth', body);
+  
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+  
+      dispatch(loadUser());
+    } catch (err) {
+      const errors = err.response.data.errors;
+  
+      if (errors) {
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+      }
+  
+      dispatch({
+        type: LOGIN_FAIL
+      });
+    }
+  };
+  
+  // Logout
+  export const logout = () => ({ type: LOGOUT });
