cd17af2d7c4a8e216dc5ceb1600bd181bebd3049
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-07-18 20:18:49 -0400

    update login actions

--- client/src/Actions/login.js
+++ client/src/Actions/login.js
@@ -18,7 +18,7 @@ export const login = (login, password) => async dispatch => {
         payload: res.data
       });
   
-      dispatch(loadUser());
+      // dispatch(loadUser());
     } catch (err) {
       const errors = err.response.data.errors;
   
